98e7cb76844bf6f97d6e5970d5e7c5458e8ed8cf
jayden-jy <121775360+jayden-jy@users.noreply.github.com>
2024-04-29 16:20:24 +0900

    Add files via upload

--- portfolio/js/ui_common.js
+++ portfolio/js/ui_common.js
@@ -1,11 +1,11 @@
 'use strict';
-
 document.addEventListener("DOMContentLoaded", function() {
   registUI();
 });
 
 function registUI() {
   if (document.querySelector('.tab_container')) { _tabContents(); } // 접근성 탭
+  if (document.querySelector('.wrap_layer')) { _layerPop(); } // 레이어팝업
 }
 
 
@@ -13,7 +13,7 @@ function registUI() {
   * @name _tabContents()
   * @description // 접근성 탭
   */
-var _tabContents = function() {
+const _tabContents = function() {
   const tabs = document.querySelectorAll('.tab');
   tabs.forEach(tab => {
     tab.addEventListener('click', function() {
@@ -57,3 +57,42 @@ var _tabContents = function() {
   // 페이지 로딩 시 첫 번째 탭 내용 표시
   document.getElementById('menu1').hidden = false;
 }
+
+/**
+  * @name _layerPop()
+  * @description // 레이어 팝업
+  */
+const _layerPop = function() {
+  const openPopupBtns = document.querySelectorAll('.openPopupBtn');
+  const closePopupBtns = document.querySelectorAll('.closePopupBtn');
+
+  function openPopup(event) {
+    event.preventDefault();
+    const targetPopupId = this.getAttribute('aria-controls');
+    const targetPopup = document.getElementById(targetPopupId);
+    if (targetPopup) {
+      targetPopup.classList.add('show');
+      document.body.classList.add('popup_open');
+      targetPopup.querySelector('.closePopupBtn').focus();
+    }
+  }
+
+  function closePopup(event) {
+    event.preventDefault();
+    const targetPopup = this.closest('.wrap_layer');
+    if (targetPopup) {
+      targetPopup.classList.remove('show');
+      document.body.classList.remove('popup_open');
+      const relatedOpenPopupBtnId = targetPopup.getAttribute('aria-labelledby');
+      const relatedOpenPopupBtn = document.querySelector(`[aria-controls="${relatedOpenPopupBtnId}"]`);
+      if (relatedOpenPopupBtn) {
+        relatedOpenPopupBtn.focus();
+      }
+    }
+  }
+
+  openPopupBtns.forEach(btn => btn.addEventListener('click', openPopup));
+  closePopupBtns.forEach(btn => btn.addEventListener('click', closePopup));
+}
+
+
